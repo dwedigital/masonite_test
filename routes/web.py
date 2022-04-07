c504be1bedbf0f6f95ab60d10c98696e863d7264
dwedigital <dave@dwedigital.com>
2022-04-07 10:31:16 +0100

    changed url to be http not https and fixed auth routes

--- routes/web.py
+++ routes/web.py
@@ -1,18 +1,13 @@
 from masonite.routes import Route
 # commented out for testing routes
-# from masonite.authentication import Auth
+from masonite.authentication import Auth
 from app.controllers.auth.RegisterController import RegisterController
 from app.controllers.auth.LoginController import LoginController
 
 ROUTES = [
     Route.get("/", "WelcomeController@show"),
     Route.get("/blog", "BlogController@show"),
-    Route.get("/login", LoginController.show).name('login'),
-    Route.post("/login", LoginController.store).name('login.store'),
-    Route.get("/logout", LoginController.logout).name('logout'),
-    Route.get("/register", RegisterController.show).name('register'),
-    Route.post("/register", RegisterController.store).name('register.store'),
     ]
 
 # Manually add routes from auth module
-# ROUTES += Auth.routes()
+ROUTES += Auth.routes()
